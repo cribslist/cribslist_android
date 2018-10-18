14ebd43015a3ab75faef5a3382d56bfedc499569
Morgantheplant <morgantheplant@gmail.com>
2018-10-18 07:55:05 -0700

    make item parcelable

--- app/src/main/java/com/codepath/cribslist/models/Item.java
+++ app/src/main/java/com/codepath/cribslist/models/Item.java
@@ -1,5 +1,8 @@
 package com.codepath.cribslist.models;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import com.codepath.cribslist.constants.ITEM_FIELD;
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -7,7 +10,7 @@ import org.json.JSONObject;
 
 import java.util.ArrayList;
 
-public class Item {
+public class Item implements Parcelable {
     private long uid;
     private String title;
     private long price;
@@ -17,8 +20,8 @@ public class Item {
     private double latitude;
     private double longitude;
     private String created;
-    private ArrayList<String> photoURLs;
     private String category;
+    private ArrayList<String> photoURLs = new ArrayList<>();
     private String thumbnailURL;
 
     public long getUid() {
@@ -27,6 +30,10 @@ public class Item {
 
     public String getTitle() {
         return title;
+    }
+
+    public long getPrice() {
+        return price;
     }
 
     public String getDescription() {
@@ -39,6 +46,14 @@ public class Item {
 
     public String getLocation() {
         return location;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
     }
 
     public String getCreated() {
@@ -57,6 +72,9 @@ public class Item {
         return thumbnailURL;
     }
 
+
+    public Item(){}
+
     public Item(JSONObject jsonObject) throws JSONException{
         this.uid = jsonObject.getLong(ITEM_FIELD.ID);
         this.title = jsonObject.getString(ITEM_FIELD.TITLE);
@@ -66,7 +84,6 @@ public class Item {
         this.created = jsonObject.getString(ITEM_FIELD.CREATED);
         this.category = jsonObject.getString(ITEM_FIELD.CATEGORY);
         JSONArray urls = jsonObject.getJSONArray(ITEM_FIELD.PHOTO_URLS);
-        photoURLs = new ArrayList<>();
         for (int i = 0; i < urls.length(); i++){
             photoURLs.add(urls.getString(i));
         }
@@ -88,4 +105,51 @@ public class Item {
         }
         return results;
     }
+
+    @Override
+    public void writeToParcel(Parcel out, int flags) {
+        out.writeLong(uid);
+        out.writeString(title);
+        out.writeLong(price);
+        out.writeString(description);
+        out.writeLong(sellerID);
+        out.writeString(category);
+        out.writeString(created);
+        out.writeStringList(photoURLs);
+        out.writeString(thumbnailURL);
+    }
+
+    private Item(Parcel in) {
+        uid = in.readLong();
+        title = in.readString();
+        price = in.readLong();
+        description = in.readString();
+        sellerID = in.readLong();
+        location = in.readString();
+        latitude = in.readDouble();
+        longitude = in.readDouble();
+        created = in.readString();
+        category = in.readString();
+        in.readStringList(photoURLs);
+        thumbnailURL = in.readString();
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    public static final Parcelable.Creator<Item> CREATOR
+            = new Parcelable.Creator<Item>() {
+        @Override
+        public Item createFromParcel(Parcel in) {
+            return new Item(in);
+        }
+
+        @Override
+        public Item[] newArray(int size) {
+            return new Item[size];
+        }
+    };
+
 }
