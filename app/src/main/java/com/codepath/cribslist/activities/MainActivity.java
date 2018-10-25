e0ec146ca95b012700d30f8b5e70dae5ece5c6da
Morgantheplant <morgantheplant@gmail.com>
2018-10-24 21:40:01 -0700

    split main view into fragments

--- app/src/main/java/com/codepath/cribslist/activities/MainActivity.java
+++ app/src/main/java/com/codepath/cribslist/activities/MainActivity.java
@@ -3,122 +3,65 @@ package com.codepath.cribslist.activities;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.GridLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Toast;
 
 import com.codepath.cribslist.R;
-import com.codepath.cribslist.adapters.ItemAdapter;
-import com.codepath.cribslist.constants.API_PARAM;
-import com.codepath.cribslist.constants.API_ROUTE;
-import com.codepath.cribslist.models.Item;
+import com.codepath.cribslist.adapters.ListingFragmentPagerAdapter;
+import com.codepath.cribslist.animations.VerticalFlipTransformation;
 import com.crashlytics.android.Crashlytics;
-import com.loopj.android.http.AsyncHttpClient;
-import com.loopj.android.http.JsonHttpResponseHandler;
-import com.loopj.android.http.RequestParams;
 
-import org.json.JSONArray;
-import org.json.JSONObject;
-
-import java.util.ArrayList;
-
-import cz.msebera.android.httpclient.Header;
 import io.fabric.sdk.android.Fabric;
 
 public class MainActivity extends AppCompatActivity {
-    int numberOfColumns = 2;
-    RecyclerView mRecyclerView;
-    RecyclerView.Adapter mAdapter;
-    ArrayList<Item> items;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         Fabric.with(this, new Crashlytics());
-        
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        setupViewPager();
+        setupFAB();
+    }
 
+    public void setupViewPager(){
+        ViewPager viewPager = findViewById(R.id.viewpager);
+        viewPager.setAdapter(new ListingFragmentPagerAdapter(getSupportFragmentManager(),
+                this));
+        TabLayout tabLayout = findViewById(R.id.sliding_tabs);
+        tabLayout.setupWithViewPager(viewPager);
+        viewPager.setPageTransformer(true, new VerticalFlipTransformation());
+
+    }
+
+    public void setupFAB(){
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                launchPostActivity();
+               launchPostActivity();
             }
         });
-
-        mRecyclerView = findViewById(R.id.items);
-
-        GridLayoutManager layoutManager = new GridLayoutManager(this, numberOfColumns);
-        mRecyclerView.setLayoutManager(layoutManager);
-        items = new ArrayList<>();
-        mAdapter = new ItemAdapter(items);
-        mRecyclerView.setAdapter(mAdapter);
-        loadNextDataFromApi(-1);
-
-//        scrollListener = new EndlessRecyclerViewScrollListener(layoutManager) {
-//            @Override
-//            public void onLoadMore(int page, int totalItemsCount, RecyclerView view) {
-//                loadNextDataFromApi(page);
-//            }
-//        };
-
-//        mRecyclerView.addOnScrollListener(scrollListener);
-    }
-
-    public void loadNextDataFromApi(int page){
-        AsyncHttpClient client = new AsyncHttpClient();
-        RequestParams params = getRequestParams(page);
-        client.get(API_ROUTE.ITEMS, params, new JsonHttpResponseHandler() {
-            @Override
-            public void onSuccess(int statusCode, Header[] headers, JSONArray response) {
-                super.onSuccess(statusCode, headers, response);
-                items.addAll(Item.fromJSONArray(response));
-                mAdapter.notifyDataSetChanged();
-            }
-
-            @Override
-            public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse) {
-                super.onFailure(statusCode, headers, throwable, errorResponse);
-                Toast.makeText(getApplicationContext(), getResources().getText(R.string.api_error), Toast.LENGTH_LONG).show();
-            }
-        });
-    }
-
-    public RequestParams getRequestParams(int page){
-        RequestParams params = new RequestParams();
-        // todo add this functionality later
-        if(page == -1){
-            return params;
-        }
-        params.put(API_PARAM.COUNT, 10);
-        params.put(API_PARAM.PAGE, page);
-        return params;
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             launchAccountActivity();
             return true;
---
+++ app/src/main/java/com/codepath/cribslist/fragments/Listings.java
@@ -0,0 +1,134 @@
+package com.codepath.cribslist.fragments;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+import com.codepath.cribslist.R;
+import com.codepath.cribslist.adapters.ItemAdapter;
+import com.codepath.cribslist.constants.API_PARAM;
+import com.codepath.cribslist.constants.API_ROUTE;
+import com.codepath.cribslist.listeners.EndlessRecyclerViewScrollListener;
+import com.codepath.cribslist.models.Item;
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.JsonHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+import cz.msebera.android.httpclient.Header;
+
+
+public class Listings extends Fragment {
+    int numberOfColumns = 2;
+    RecyclerView mRecyclerView;
+    RecyclerView.Adapter mAdapter;
+    ArrayList<Item> items = new ArrayList<>();
+    private static final String LISTING_TYPE = "LISTING_TYPE";
+    private int listingType;
+    private EndlessRecyclerViewScrollListener scrollListener;
+
+    public Listings() {
+        // Required empty public constructor
+    }
+
+    public static Listings newInstance(int i) {
+        Bundle args = new Bundle();
+        args.putInt(LISTING_TYPE, i);
+        Listings fragment = new Listings();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            listingType = getArguments().getInt(LISTING_TYPE);
+        }
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_listings, container, false);
+        mRecyclerView = v.findViewById(R.id.items);
+
+        GridLayoutManager layoutManager = new GridLayoutManager(getContext(), numberOfColumns);
+        mRecyclerView.setLayoutManager(layoutManager);
+        mAdapter = new ItemAdapter(items);
+        mRecyclerView.setAdapter(mAdapter);
+        loadNextDataFromApi(getRouteForType(listingType), -1);
+
+        scrollListener = new EndlessRecyclerViewScrollListener(layoutManager) {
+            @Override
+            public void onLoadMore(int page, int totalItemsCount, RecyclerView view) {
+                // todo: implement endless scrolling with API
+                //loadNextDataFromApi(page);
+            }
+        };
+        mRecyclerView.addOnScrollListener(scrollListener);
+
+        return v;
+    }
+
+    public void addItems(JSONArray response){
+
+        items.addAll(Item.fromJSONArray(response));
+        mAdapter.notifyDataSetChanged();
+    }
+
+    public String getRouteForType(int type){
+        switch(type){
+            case 0:
+                return API_ROUTE.ITEMS;
+            case 1:
+                //TODO: grab id from user
+                return String.format(API_ROUTE.MY_ITEMS, 123);
+            default:
+                return API_ROUTE.ITEMS;
+        }
+    }
+
+    public void loadNextDataFromApi(String route, int page){
+        AsyncHttpClient client = new AsyncHttpClient();
+        RequestParams params = getRequestParams(page);
+        client.get(route, params, new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONArray response) {
+                super.onSuccess(statusCode, headers, response);
+                addItems(response);
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse) {
+                super.onFailure(statusCode, headers, throwable, errorResponse);
+                Toast.makeText(getContext(), getResources().getText(R.string.api_error), Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    public RequestParams getRequestParams(int page){
+        RequestParams params = new RequestParams();
+        // todo add this functionality later
+        if(page == -1){
+            return params;
+        }
+        // todo: pass last id,  faster mongo query would be:
+        // users = Users.find({'_id'> last_id}).limit(10);
+        params.put(API_PARAM.COUNT, 10);
+        params.put(API_PARAM.PAGE, page);
+        return params;
+    }
+
+
+}
